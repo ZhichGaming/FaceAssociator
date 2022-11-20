8e73f5f2d08feccf64a93a1e5229433ceb4c3091
ZhichGaming <nick.zhicheng@gmail.com>
2022-11-20 01:32:37 -0500

    Initial Commit

---
+++ FaceAssociator/FaceAssociatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAssociatorApp.swift
+//  FaceAssociator
+//
+//  Created by Nick on 2022-11-20.
+//
+
+import SwiftUI
+
+@main
+struct FaceAssociatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
